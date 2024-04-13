6f04f2c2384d07187cd014dda6a3c48cda381cd1
GabrielFunatsu <gabrielfunatsu@gmail.com>
2024-04-13 20:24:03 -0300

    Home page

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../Navbar";
+import styles from "./styles.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+    </div>
+  );
+};
+
+export { Home };
