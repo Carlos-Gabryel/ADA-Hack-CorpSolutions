335d79158ae47107792bc232371a2f698bc7c339
Gustavo <gustavoob06@gmail.com>
2024-04-15 22:47:38 -0300

    pagina de login

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Login } from './pages/Login/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="/login" element={<App />} /> */}
+        <Route path="/login" element={<Login />} />
         
         {/* ROTA PRIVADA */}
         {/* <Route path="/painel" element={<Panel />} /> */}
